7c4273f1a28fc9b1d83d992f30d9f5ddc6e48cc7
Chris <dogbot460@gmail.com>
2026-06-08 12:38:51 -0400

    data set

--- data.js
+++ data.js
@@ -1,14 +1,155 @@
-const data = [
+
+//Stores different array values for SCALABILITYY
+const vehicleData = [
+
+  {
+    year: 2011,
+    make: "RAM",
+    model: "1500",
+    productType: "Front Bumper", 
+  },
+  {
+    year: 2012,
+    make: "RAM",
+    model: "1500",
+    productType: "Front Bumper",   
+  },
+  {
+    year: 2013,
+    make: "RAM",
+    model: "1500",
+    productType: "Front Bumper",  
+  },
+  {
+    year: 2014,
+    make: "RAM",
+    model: "1500",
+    productType: "Front Bumper",  
+  },
   {
     year: 2015,
     make: "RAM",
     model: "1500",
-    productTypes: ["Front Bumper", "Rear Bumper"]
+    productType: "Front Bumper", 
+  },
+  {
+    year: 2011,
+    make: "RAM",
+    model: "1500",
+    productType: "Rear Bumper",
+  },
+  {
+    year: 2012,
+    make: "RAM",
+    model: "1500",
+    productType: "Rear Bumper",
+  },
+  {
+    year: 2013,
+    make: "RAM",
+    model: "1500",
+    productType: "Rear Bumper",
+  },
+  {
+    year: 2014,
+    make: "RAM",
+    model: "1500",
+    productType: "Rear Bumper", 
   },
   {
     year: 2015,
-    make: "TOYOTA",
-    model: "Tacoma",
-    productTypes: ["Front Bumper"]
+    make: "RAM",
+    model: "1500",
+    productType: "Rear Bumper",
+  },
+  {
+    year: 2012,
+    make: "RAM",
+    model: "2500",
+    productType: "Tailgate",
+  },
+  {
+    year: 2013,
+    make: "RAM",
+    model: "2500",
+    productType: "Tailgate",
+  },
+  {
+    year: 2014,
+    make: "RAM",
+    model: "2500",
+    productType: "Tailgate",
+  },
+  {
+    year: 2015,
+    make: "RAM",
+    model: "2500",
+    productType: "Tailgate",
+  },
+  {
+    year: 2016,
+    make: "RAM",
+    model: "2500",
+    productType: "Tailgate",
+  },
+  {
+    year: 2013,
+    make: "Toyota",
+    model: "Camry",
+    productType: "Front Bumper", 
+  },
+  {
+    year: 2014,
+    make: "Toyota",
+    model: "Camry",
+    productType: "Front Bumper",
+  },
+  {
+    year: 2015,
+    make: "Toyota",
+    model: "Camry",
+    productType: "Front Bumper",
+  },
+  {
+    year: 2016,
+    make: "Toyota",
+    model: "Camry",
+    productType: "Front Bumper", 
+  },
+  {
+    year: 2017,
+    make: "Toyota",
+    model: "Corolla",
+    productType: "Front Bumper", 
+  },
+  {
+    year: 2013,
+    make: "Toyota",
+    model: "Corolla",
+    productType: "Passenger Side Fender", 
+  },
+  {
+    year: 2014,
+    make: "Toyota",
+    model: "Corolla",
+    productType: "Passenger Side Fender",   
+  },
+  {
+    year: 2015,
+    make: "Toyota",
+    model: "Corolla",
+    productType: "Passenger Side Fender",   
+  },
+  {
+    year: 2016,
+    make: "Toyota",
+    model: "Corolla",
+    productType: "Passenger Side Fender",
+  },
+  {
+    year: 2017,
+    make: "Toyota",
+    model: "Corolla",
+    productType: "Passenger Side Fender",
   }
-];
+]
