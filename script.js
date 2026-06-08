1a7c1a3f3f3647d3aac12c6e780f2596a74339bf
Chris <dogbot460@gmail.com>
2026-06-08 16:01:11 -0400

    MORE WORRK DONNE - Did more with dataset -Created Year and Make arrays with no duplicates

--- script.js
+++ script.js
@@ -15,6 +15,34 @@ for (let i = 0; i < Upd_year.length; i++) {
 }
 
 
+let SelectedYear;
+
+
+//Filters out the appropriate sequence vehicles
+
+document.getElementById("Year").addEventListener("change", (e) => {
+
+  SelectedYear = e.target.value; //Store whatever the user has selected
+
+
+  //Only choose the data that aligns with the picked year
+  const Filter_Vehicle = vehicleData.filter(vehicle => vehicle.year== SelectedYear); 
+
+
+  //Turn data into an array
+  const makes = Filter_Vehicle.map(vehicle => vehicle.make);
+  
+  //Create a set that has no duplicates
+  const Upd_makes = [...new Set(makes)];
+  console.log(Upd_makes);
+
+  
+
+});
+
+
+
+
 
   
 
