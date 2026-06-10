6ef17a822513d3d46366507ff5a3272976d32325
Chris <dogbot460@gmail.com>
2026-06-10 08:22:29 -0400

    I think comments done, getting ready to submit.

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ SelectModel.disabled = true;
 SelectProduct.disabled = true;
 
 
-// Turns only year data into a Set
+// Turns only year data into an array
 // Contains all years found in data set
 const years = vehicleData.map(vehicle => vehicle.year);
 
@@ -46,7 +46,7 @@ let SelectedProduct;
 
 
 //Filters out the appropriate sequence vehicles
-// Creates Make options using Year listener
+// Creates and Displays Make options using Year listener
 
 document.getElementById("Year").addEventListener("change", (e) => {
 
@@ -68,7 +68,6 @@ document.getElementById("Year").addEventListener("change", (e) => {
 
   if (SelectModel.length != 1) {
 
-    //reset the element select
     SelectModel.innerHTML = 
     `<option value="" selected disabled>Select Model</option>`;
 
@@ -76,7 +75,6 @@ document.getElementById("Year").addEventListener("change", (e) => {
 
   if (SelectProduct.length != 1) {
 
-    //reset the element select
     SelectProduct.innerHTML = 
     `<option value="" selected disabled>Select Product</option>`;
 
@@ -98,6 +96,8 @@ document.getElementById("Year").addEventListener("change", (e) => {
   console.log("Make Set is:", Upd_makes);
 
   
+  /*Same formula to populate options, except this
+    time only for makes that have the selected year */
 
   for (let i = 0; i < Upd_makes.length; i++) {
 
@@ -113,35 +113,40 @@ document.getElementById("Year").addEventListener("change", (e) => {
 
 
 
-//Models select script
+// Creates and Displays Model options using the Make listener
 
 document.getElementById("Make").addEventListener("change", (e) => {
 
 
+  /* Only enable Model while keeping product disabled.
+     Just in case user tries to access Product before
+     selecting a model.   */
+
   SelectModel.disabled = false;
   SelectProduct.disabled = true;
+
   
+    /* Whenever make is changed, only Model and 
+        product reset  */
 
   if (SelectModel.length != 1) {
 
-    //reset the element select
     SelectModel.innerHTML = 
     `<option value="" selected disabled>Select Model</option>`;
-
   }
 
   if (SelectProduct.length != 1) {
 
-    //reset the element select
     SelectProduct.innerHTML = 
     `<option value="" selected disabled>Select Product</option>`;
 
   }
 
-
+  /* Stops any confusion of value type and forces
+     it to String. To Simplify */
   SelectedMake = String(e.target.value);
 
-  //Filter only data that contains the selected year and make
+  //Grab data that only matches the selected year and make
   const Filter_Make = vehicleData.filter(vehicle =>
     vehicle.year === SelectedYear &&
     vehicle.make === SelectedMake
@@ -149,6 +154,8 @@ document.getElementById("Make").addEventListener("change", (e) => {
   );
 
 
+  /* Same array and Set Sequence like in year and makes, except 
+     this time doing models */
 
   const models = Filter_Make.map(vehicle => vehicle.model);
 
@@ -169,47 +176,70 @@ document.getElementById("Make").addEventListener("change", (e) => {
 });
 
 
-  //ProductType Select script
+
+  // Creates and Displays Product options using the Model listener
+
   document.getElementById("Model").addEventListener("change", (e) => {
 
+    /* Once Model is chosen, the last select, Product, is finally
+       enabled */
+
     SelectProduct.disabled = false;
-    
-
-  if (SelectProduct.length != 1) {
-
-    //reset the element select
-    SelectProduct.innerHTML = 
-    `<option value="" selected disabled>Select Product</option>`;
-
-  }
-
-  SelectedModel = String(e.target.value);
-
-  const Filter_Model = vehicleData.filter(vehicle =>
-    vehicle.year === SelectedYear &&
-    vehicle.make === SelectedMake &&
-    vehicle.model === SelectedModel
-
-  );
-
-  const products = Filter_Model.map(vehicle => vehicle.productType);
-
-  const Upd_products = [...new Set(products)];
-
-  console.log("Product Set is:", Upd_products);
 
 
-  for (let i = 0; i < Upd_products.length; i++) {
+    /* Only Product is reset when user changes the model*/
 
-    const Product_opt = document.createElement("option");
-    Product_opt.value = Upd_products[i];
-    Product_opt.textContent = Upd_products[i];
-    SelectProduct.appendChild(Product_opt);
+    if (SelectProduct.length != 1) {
 
-  }
+        SelectProduct.innerHTML = 
+        `<option value="" selected disabled>Select Product</option>`;
+
+    }
+
+    /* Also turn model into string for easability,
+       and bc Model has choice number or words in data */
+
+    SelectedModel = String(e.target.value);
+
+    /* Grab only data that aligns with all three previous 
+       choices (Year, Make, and Model )*/
+
+    const Filter_Model = vehicleData.filter(vehicle =>
+        vehicle.year === SelectedYear &&
+        vehicle.make === SelectedMake &&
+        vehicle.model === SelectedModel
+
+    );
+
+
+    /* Same array and Set Sequence like in year, makes, and models, 
+       except this time doing products */
+
+    const products = Filter_Model.map(vehicle => vehicle.productType);
+
+    const Upd_products = [...new Set(products)];
+
+    console.log("Product Set is:", Upd_products);
+
+
+    for (let i = 0; i < Upd_products.length; i++) {
+
+        const Product_opt = document.createElement("option");
+        Product_opt.value = Upd_products[i];
+        Product_opt.textContent = Upd_products[i];
+        SelectProduct.appendChild(Product_opt);
+
+        }
 
 
 });
+
+
+
+/* Since product is already ready to be displayed from the Model
+   listener, the product listener is only to get store the user 
+   picked product value; and to convert to string. Also no resets
+   since product dropdown is the last one. */ 
 
 document.getElementById("Product").addEventListener("change", (e) => {
 
@@ -218,15 +248,20 @@ document.getElementById("Product").addEventListener("change", (e) => {
 });
 
 
-//Generate the URLS
+//Generate the URLS based on what the user has picked
+
 document.getElementById("Vehicle_Form").addEventListener("submit", (e) => {
 
-  //Prevents form from trying to go to the URL itself
+
+  //Prevents the form from trying to go to the URL itself
+
   e.preventDefault();
 
   console.log("Form submitted!");
 
+
   //Method for finding data that aligns with users choices
+
   const Check_Vehicle = vehicleData.find(vehicle =>
 
     vehicle.year === SelectedYear &&
@@ -236,24 +271,36 @@ document.getElementById("Vehicle_Form").addEventListener("submit", (e) => {
 
   );
 
+
+  /* Checks if vehicle exists in data-base, if so then it goes to make url,
+     if not then displays error for user. */
+
   if (Check_Vehicle) {
+
+
+    /* Makes sure that the Model used for the URL matches 
+       and is able to be used (Url doesn't take spaces to well) */
 
     const Url_Model = SelectedModel.replaceAll(" ","+");
     const Url_Product = SelectedProduct.replaceAll(" ", "+");
     
-    
+    /* Wasn't sure whether to lowercase variable,'SelectedMake' since both lead to same Url.
+       decided to not lowercase. */
+
     const Vehicle_Url = `https://partifyusa.com/collections/${SelectedYear}-${SelectedMake}-${Url_Model}?filter.p.product_type=${Url_Product}`;
     
     
-    // https://partifyusa.com/collections/2016-Toyota-Camry?filter.p.product_type=Front+Bumper
+    // Example url: https://partifyusa.com/collections/2016-Toyota-Camry?filter.p.product_type=Front+Bumper
 
     
-
+    /* Once Url is formed, makes sure the add vehicle button 
+       sends user to url */
     window.location.href = Vehicle_Url;
 
   }
   else {
 
+    alert("Error: No Vehicle found");
     console.log("Error: No Vehicle found")
   }
 
